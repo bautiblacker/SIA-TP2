80fe98f988fcd8e867d6b379aa8c4388f569d80b
Ignacio <igrasso@itba.edu.ar>
2020-09-06 18:10:32 -0300

    random get N

--- src/SelectionResolver.java
+++ src/SelectionResolver.java
@@ -22,16 +22,16 @@ public class SelectionResolver {
     public static void main(String[] args) {
         JSONParser parser = new JSONParser();
         String filePath = args[0];
-        String configPath = args[1];
+//        String configPath = args[1];
         Map<EquipmentType, List<Equipment>> equipments = new HashMap<>();
         List<Player> starterPopulation;
         try {
             // deberia ser un while
             Reader reader = new FileReader(filePath);
             EquipmentType equipmentType = FileParser.equipmentTypeByFileName(filePath);
-            Data data = ConfigParser.parse(configPath);
+//            Data data = ConfigParser.parse(configPath);
 
-            List<Equipment> equipmentList = EquipmentParser.equipmentParser(reader, equipmentType, data.getPopulation());
+            List<Equipment> equipmentList = EquipmentParser.equipmentParser(reader, equipmentType, 10, 20);
             equipments.put(equipmentType, equipmentList);
 
             // llamar a PreEngine -> devuelve lista de players <== population
--- src/parsers/EquipmentParser.java
+++ src/parsers/EquipmentParser.java
@@ -9,20 +9,29 @@ import java.io.Reader;
 import java.util.*;
 
 public class EquipmentParser {
-    public static List<Equipment> equipmentParser(Reader reader, EquipmentType equipmentType, long number) {
+    public static List<Equipment> equipmentParser(Reader reader, EquipmentType equipmentType, long equipmentsNumber,
+                                                  int fileLines) {
         List<Equipment> equipmentList = new ArrayList<>();
         try {
             Set<Attribute> titles = titleParser(reader);
             Map<Attribute, Double> values;
+            SortedSet<Integer> randomIndexes = new TreeSet<>();
+            Random random = new Random();
+            int linesRead = 0;
+
+            while (randomIndexes.size() < equipmentsNumber) {
+                randomIndexes.add(random.nextInt(fileLines) + 1);
+            }
 
             do {
-                values = valuesParser(reader, titles);
-                Equipment equipment = getEquipment(values,equipmentType);
-                if(equipment == null){
-                    return null;
+                values = valuesParser(reader, titles, randomIndexes.first().equals(linesRead));
+                if (!values.isEmpty()) {
+                    Equipment equipment = getEquipment(values, equipmentType);
+                    equipmentList.add(equipment);
+                    randomIndexes.remove(randomIndexes.first());
                 }
-                equipmentList.add(equipment);
-            } while (!values.isEmpty());
+                linesRead++;
+            } while (equipmentList.size() <= equipmentsNumber && randomIndexes.size() > 0);
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -51,25 +60,29 @@ public class EquipmentParser {
         return lineParsed;
     }
 
-    private static Map<Attribute, Double> valuesParser(Reader reader, Set<Attribute> titles) throws IOException {
+    private static Map<Attribute, Double> valuesParser(Reader reader, Set<Attribute> titles, boolean parse) throws IOException {
         int c;
         Iterator<Attribute> iterator = titles.iterator();
         Map<Attribute, Double> values = new LinkedHashMap<>();
         StringBuilder value = new StringBuilder();
         Attribute title;
         while ((c = reader.read()) != '\n') {
-            if (c == '\t') {
-                title = iterator.next();
-                double valueDouble = Double.parseDouble(value.toString());
-                values.put(title,valueDouble);
-                value = new StringBuilder();
-            } else {
-                value.append((char) c);
+            if (parse) {
+                if (c == '\t') {
+                    title = iterator.next();
+                    double valueDouble = Double.parseDouble(value.toString());
+                    values.put(title, valueDouble);
+                    value = new StringBuilder();
+                } else {
+                    value.append((char) c);
+                }
             }
         }
-        title = iterator.next();
-        double valueDouble = Double.parseDouble(value.toString());
-        values.put(title, valueDouble);
+        if (parse) {
+            title = iterator.next();
+            double valueDouble = Double.parseDouble(value.toString());
+            values.put(title, valueDouble);
+        }
         return values;
     }
 }
